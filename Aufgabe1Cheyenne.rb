6183c8d05f59293025b917abb5855c05303d186f
CheyenneJirout <57531809+CheyenneJirout@users.noreply.github.com>
2020-03-28 11:49:24 +0100

    Add files via upload

---
+++ Aufgabe1Cheyenne.rb
@@ -0,0 +1,12 @@
+class Aufgabe1Cheyenne
+  #Als Arzt möchte ich einen Zahlenwert 
+  #in ein Textfeld eingeben können, 
+  #um die Größe der Wunde in qcm dokumentieren zu können
+
+  #Eingabe der Zahlenwerte
+  
+  print "Bitte geben Sie die Größe der Wunde in qcm ein: "
+  groesse = gets
+  puts "#{groesse}"
+  
+end
